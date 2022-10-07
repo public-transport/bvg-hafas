32586e6d24dd3d80c1e4d6d1df6e67a307503869
Jannis R <mail@jannisr.de>
2022-10-07 13:17:41 +0200

    allow customising the hafas-client profile

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
 const createHafasClient = require('hafas-client')
 const bvgProfile = require('hafas-client/p/bvg')
 
-const createClient = (userAgent) => {
-	return createHafasClient(bvgProfile, userAgent)
+const defaults = {
+	profile: bvgProfile
 }
 
+const createClient = (userAgent, opt = {}) => {
+	const {
+		profile,
+	} = {...defaults, ...opt}
+
+	return createHafasClient(profile, userAgent)
+}
+
+createClient.defaults = defaults
 module.exports = createClient
